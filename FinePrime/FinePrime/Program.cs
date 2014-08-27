7c600d4cc1ca1c422552b76329c250697821353a
SeedPaths-User <lessdismissive@email.com>
2014-08-27 14:50:02 -0600

    Improved interface, user input ability added

--- FinePrime/FinePrime/Program.cs
+++ FinePrime/FinePrime/Program.cs
@@ -10,8 +10,23 @@ namespace FinePrime
     {
         static void Main(string[] args)
         {
-            int wantPrime = 10001;
-            Console.WriteLine(loopCheck(wantPrime - 1));
+            Greet();
+            bool exit = false;
+            while (!exit)
+            {
+                string inputString = Console.ReadLine();
+                if (inputString != "exit" && inputString != "Exit" && inputString != "e" && inputString != "E")
+                {
+                    int inputInt = Convert.ToInt32(inputString);
+                    int wantPrime = inputInt;
+                    Console.WriteLine(loopCheck(wantPrime));
+                }
+                else
+                {
+                    exit = true;
+                }
+            }
+            Console.WriteLine("Thank you for using the program");
             Console.ReadKey();
         }
 
@@ -19,7 +34,7 @@ namespace FinePrime
         {
             int primeCount = 4;
             int currentNumCheck = 11;
-            while (primeCount <= end)
+            while (primeCount < end)
             {
                 if (checkPrime(currentNumCheck) == true)
                 {
@@ -50,6 +65,13 @@ namespace FinePrime
             }
             isPrime = (mayBePrime && !isNotPrime);
             return isPrime;
+        }
+
+        static void Greet()
+        {
+            Console.WriteLine("What prime do you want?");
+            Console.WriteLine("WARNING: Inputs less than 5 will not desplay correct answers\nWARNING: Inputs over 10,0000 may take a long time");
+            Console.WriteLine("Type 'exit' or 'e' to end program");
         }
     }
 }

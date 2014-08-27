8f95737509948683c51f820f5f1ef4f17095a643
SeedPaths-User <lessdismissive@email.com>
2014-08-27 13:19:43 -0600

    Finds the nth prime number

---
+++ FinePrime/FinePrime/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinePrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wantPrime = 1000000;
+            Console.WriteLine(loopCheck(wantPrime - 1));
+            Console.ReadKey();
+        }
+
+        static int loopCheck(int end)
+        {
+            int primeCount = 4;
+            int currentNumCheck = 11;
+            while (primeCount <= end)
+            {
+                if (checkPrime(currentNumCheck) == true)
+                {
+                    primeCount++;
+                    //Take out in final version, testing purposes only
+                    Console.WriteLine(currentNumCheck);
+                }
+                currentNumCheck += 2;
+            }
+            return (currentNumCheck - 2);
+        }
+
+        static bool checkPrime(int number)
+        {
+            bool mayBePrime = false;
+            bool isNotPrime = false;
+            bool isPrime = false;
+            for (int i = 3; i <= (Math.Floor(Math.Sqrt(number))) && !isNotPrime; i += 2)
+            {
+                if (number % i == 0)
+                {
+                    isNotPrime = true;
+                }
+                else
+                {
+                    mayBePrime = true;
+                }
+            }
+            isPrime = (mayBePrime && !isNotPrime);
+            return isPrime;
+        }
+    }
+}
